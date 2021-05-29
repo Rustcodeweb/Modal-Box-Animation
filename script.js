677f5554c4873b8f9784930424cbb9b097f951a7
Rustcode <rustcode95@gmail.com>
2021-05-29 18:11:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var modal = document.getElementById('modal');
+function openModal() {
+    modal.style.display = "block";
+}
+function closeModal() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event){
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
